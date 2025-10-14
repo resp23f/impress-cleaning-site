3255dad45540ecdde737d992399e7ec2de57505c
Alex Hoffman <alexanderhoffman@Mac.attlocal.net>
2025-10-14 00:47:22 -0500

    Remove old page-level footer; use global Footer component

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,7 @@ export default function RootLayout({ children }) {
 <body className="min-h-screen flex flex-col bg-gradient-to-b from-white to-[#fdf9f3]">
   <main className="flex-1">{children}</main>
   <Footer />
-</body>    </html>
+</body>    
+</html>
   );
 }
