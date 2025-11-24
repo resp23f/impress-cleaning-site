19f16e21903be9740a6c765d16c5cdd1147af47a
Alex Hoff <ahoffman188@gmail.com>
2025-11-24 01:43:22 -0600

    feat: update next.config.mjs to include experimental dynamicIO configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,12 @@ const nextConfig = {
   allowedDevOrigins: [
     'http://192.168.68.83:3000', // your Mac's IP (from earlier)
     'http://localhost:3000'
-  ],
+ ],
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    dynamicIO: true,
+  },
 };
-
 export default nextConfig;
