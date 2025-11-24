b0b8561ff73efddf60bcf27d7f6699b32c1e46c0
Alex Hoff <ahoffman188@gmail.com>
2025-11-24 01:31:53 -0600

    feat: add eslint configuration to ignore warnings during builds

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     'http://192.168.68.83:3000', // your Mac's IP (from earlier)
     'http://localhost:3000'
   ],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
