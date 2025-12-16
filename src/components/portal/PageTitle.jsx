5fd30be787fffaf304d07c39b9d24fdf6b5756ac
Alex Hoff <ahoffman188@gmail.com>
2025-12-16 13:32:55 -0600

    refactor(PageTitle): add console logs for debugging title updates

--- src/components/portal/PageTitle.jsx
+++ src/components/portal/PageTitle.jsx
@@ -3,7 +3,9 @@ import { useEffect } from 'react'
 
 export default function PageTitle({ title }) {
   useEffect(() => {
+    console.log('PageTitle running with:', title)
     document.title = `${title} | Impress Cleaning Services`
+    console.log('Title set to:', document.title)
   }, [title])
 
   return null
